0033b4ed7a05791eba5e103205c7e8c0a271cb74
Joy Bappy <joybappy2025@gmail.com>
2026-09-15 12:30:05 +0600

    problem 4 solved

--- answer.js
+++ answer.js
@@ -39,7 +39,7 @@ function getDayType(day) {
   }
 }
 
-// problem 2 solution
+// problem 3 solution
 function validateUsername(username) {
   const usernameLower = username.toLowerCase();
 
@@ -54,4 +54,32 @@ function validateUsername(username) {
   } else {
     return "Available";
   }
+}
+
+// problem 4 solution
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+  let fare = 0;
+
+  if (distance > 0) {
+    if (distance <= 2) {
+      fare = 50;
+    }
+    if (distance > 2) {
+      const fareForFirst2KM = 50;
+      const fareAfterFirst2KM = (distance - 2) * 15;
+      fare = fareForFirst2KM + fareAfterFirst2KM;
+    }
+
+    if (waitingMinutes > 0) {
+      const fairForWaiting = waitingMinutes * 2;
+      fare += fairForWaiting;
+    }
+
+    if (isNight) {
+      const extraChargeForNight = (fare * 20) / 100;
+      fare += extraChargeForNight;
+    }
+  }
+
+  return fare;
 }
