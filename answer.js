46ae80b27fa9c695111973a90af6ab14074529cd
Joy Bappy <joybappy2025@gmail.com>
2026-09-15 10:02:39 +0600

    problem 2 solved

--- answer.js
+++ answer.js
@@ -1,5 +1,40 @@
 // problem 1 solution
-function describeValue(value){
-  return `${typeof(value)} | ${value? "truthy": "falsy"}`
+function describeValue(value) {
+  return `${typeof value} | ${value ? "truthy" : "falsy"}`;
 }
 
+// problem 2 solution
+function getDayType(day) {
+  switch (day.toLowerCase()) {
+    case "saturday":
+      return "Weekend";
+      break;
+
+    case "sunday":
+      return "Working Day";
+      break;
+
+    case "monday":
+      return "Working Day";
+      break;
+
+    case "tuesday":
+      return "Working Day";
+      break;
+
+    case "wednesday":
+      return "Working Day";
+      break;
+
+    case "thursday":
+      return "Working Day";
+      break;
+
+    case "friday":
+      return "Weekend";
+      break;
+
+    default:
+      return "Invalid Day";
+  }
+}
